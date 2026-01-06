ee56dcd41addf37ee1a789beacb6398b118558e9
mickBoat00 <m.boateng0000@gmail.com>
2026-01-06 10:59:50 +0000

    change s3 name

--- infra/main.tf
+++ infra/main.tf
@@ -1,6 +1,6 @@
 module "public_s3" {
   source      = "./modules/s3"
-  bucket_name = "tutorials-storage-${var.account_id}"
+  bucket_name = "esc-uploads-${var.account_id}"
   cors_allowed_methods = ["GET", "PUT", "POST", "HEAD"]
   cors_allowed_origins = [
       "http://localhost:5174",
