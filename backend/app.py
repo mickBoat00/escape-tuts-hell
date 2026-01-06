22dd022c0675a522aad5ec2842eaa9217f978601
mickBoat00 <m.boateng0000@gmail.com>
2026-01-06 10:10:11 +0000

    modify backend code

--- backend/app.py
+++ backend/app.py
@@ -6,25 +6,46 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 
 from botocore.exceptions import ClientError
+from botocore.config import Config
 
 from dotenv import load_dotenv
+
+from pydantic import BaseModel
+
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI(title="Podcast AI Backend")
 
+origins = [
+    os.environ["FRONTEND_URL"]
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 load_dotenv()
 
-def create_presigned_post(
-    bucket_name, object_name, fields=None, conditions=None, expiration=3600
-):
-    s3_client = boto3.client('s3')
+def create_presigned_url(bucket_name, object_name, content_type, expiration=3600):
+    s3_client = boto3.client('s3',
+        region_name=os.environ["AWS_REGION"], 
+        config=Config(signature_version='s3v4')
+    )
     try:
-        response = s3_client.generate_presigned_post(
-            bucket_name,
-            object_name,
-            Fields=fields,
-            Conditions=conditions,
-            ExpiresIn=expiration,
+        response = s3_client.generate_presigned_url(
+            'put_object',
+            Params={
+                'Bucket': bucket_name,
+                'Key': object_name,
+                'ContentType': content_type
+            },
+            ExpiresIn=expiration
         )
     except ClientError as e:
         logging.error(e)
@@ -32,16 +53,27 @@ def create_presigned_post(
 
     return response
 
+
 class FileDataRequest(BaseModel):
     fileName: str
     fileSize: int
     fileDuration: int
+    contentType: str
 
 
 @app.post("/upload")
 def presigned_token(request: FileDataRequest):
-    logging.info(f"fileName: {request.filename} fileSize: {request.fileSize} fileDuration: {request.fileDuration}")
-    return create_presigned_post(
-        bucket_name = os.environ['S3_BUCKET_NAME'],
-        object_name = "uploads/"
+    logging.info(f"fileName: {request.fileName} fileSize: {request.fileSize} fileDuration: {request.fileDuration}")
+
+    object_name = f"uploads/{request.fileName}"
+
+    url = create_presigned_url(
+        bucket_name=os.environ['S3_BUCKET_NAME'],
+        object_name=object_name,
+        content_type=request.contentType
     )
+    
+    if url is None:
+        raise HTTPException(status_code=500, detail="Failed to generate presigned URL")
+    
+    return {"url": url}
