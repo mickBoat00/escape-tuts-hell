6196c62243f52a0172d2c4d2d29764fad63cf931
mickBoat00 <m.boateng0000@gmail.com>
2026-01-14 16:05:21 +0000

    add public read access to bucket objects

--- infra/modules/s3/main.tf
+++ infra/modules/s3/main.tf
@@ -11,6 +11,26 @@ resource "aws_s3_bucket_public_access_block" "this" {
   restrict_public_buckets = false
 }
 
+resource "aws_s3_bucket_policy" "public_read" {
+  bucket = aws_s3_bucket.uploads.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.uploads.arn}/*"
+      }
+    ]
+  })
+
+  depends_on = [aws_s3_bucket_public_access_block.this]
+}
+
+
 resource "aws_s3_bucket_cors_configuration" "this" {
   count  = var.cors_rule == null ? 0 : 1
   bucket = aws_s3_bucket.uploads.id
